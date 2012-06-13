7905f4911334614eb6c03f894c77a737932ed46d
shiren1118 <shiren1118@126.com>
2012-06-14 07:45:25 +0800

    test commdn

--- src/cli.rb
+++ src/cli.rb
@@ -15,20 +15,29 @@ opt_parser = OptionParser.new do |opt|
   opt.banner = "Usage: ityran tools COMMAND [OPTIONS]"
   opt.separator  ""
   opt.separator  "Commands"
-  opt.separator  "     init: start server"
-  opt.separator  "     page: stop server"
-  opt.separator  "     restart: restart server"
-  opt.separator  ""
+  opt.separator  "     init           : 初始化目录"
+  opt.separator  "     init article   : 初始化目录和对应md文件"
+  opt.separator  "     download       : 下载文本内容"
+  opt.separator  "     page url       : 初始化对应md文件"
+  opt.separator  "     build          : 生成文档"
+  opt.separator  "     clean          : 清理"
+  opt.separator  "     help           : 帮助"
+  opt.separator  "     version        : 查看版本"
   opt.separator  "Options"
 
-  opt.on("-e","--environment ENVIRONMENT","which environment you want server run") do |environment|
-    options[:environment] = environment
+  opt.on("-article","--article articleName","which ${articleName} you want init md with name") do |article|
+    puts article
+    options[:article] = article
   end
 
-  opt.on("-d","--daemon","runing on daemon mode?") do
+  opt.on("-name","--daemon","runing on daemon mode?") do
     options[:daemon] = true
   end
 
+  opt.on("-v","--version","version") do 
+    puts "ityran build tools v1.0"
+  end
+    
   opt.on("-h","--help","help") do
     puts opt_parser
   end
@@ -37,12 +46,18 @@ end
 opt_parser.parse!
 
 case ARGV[0]
-when "start"
-  puts "call start on options #{options.inspect}"
-when "stop"
+when "init"
+  puts "call init on options #{options.inspect}"
+when "download"
   puts "call stop on options #{options.inspect}"
-when "restart"
+when "build"
   puts "call restart on options #{options.inspect}"
+when "clean"
+  puts "call restart on options #{options.inspect}"
+when "page"
+  puts "call restart on options #{options.inspect}"
+when "version"
+  puts "ityran build tools v1.0"
 else
   puts opt_parser
 end
---
+++ src/command.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'date'
+
+options = {}
+
+opt_parser = OptionParser.new do |opt|
+  opt.banner = "Usage: ityran COMMAND [OPTIONS]"
+  opt.separator  ""
+  opt.separator  "Commands"
+  opt.separator  "     init     : init resources dir"
+  opt.separator  "     build    : build and create pdf and html docs"
+  opt.separator  "     clean    : clear no use resources"
+  opt.separator  ""
+  opt.separator  "Options"
+  
+  opt.on("-e ENVIRONMENT","--environment ENVIRONMENT",Numeric,"which enviroment you want server run") do |environment|
+    options[:environment] = environment
+  end
+  
+  opt.on("-d","--daemon","runing on daemon mode?") do
+    options[:daemon] = true
+  end
+  
+  opt.on("--delay N", Float, "Delay N seconds before executing") do |n|
+    options[:delay] = n
+  end
+  
+  opt.on("-j x,y,z","--jurisdictions x,y,z", Array, "which jurisdiction will start") do |jurisdictions|
+    options[:jurisdictions] = jurisdictions
+  end
+  
+  server_list = %w[a b c]
+  opt.on("-s SERVERS","--servers SERVERS", server_list, "which server will start between #{server_list.join(',')}") do |servers|
+    options[:servers] = servers
+  end
+ 
+  opt.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+  
+  opt.on_tail("-h","--help","help") do 
+    puts opt_parser
+  end
+end
+
+opt_parser.parse!
+
+case ARGV[0]
+when "start" 
+  puts "call start on options #{options.inspect}"
+when "stop"
+  puts "call stop on options #{options.inspect}"
+when "restart"
+  puts "call restart on options #{options.inspect}"
+else 
+  puts opt_parser
+end
